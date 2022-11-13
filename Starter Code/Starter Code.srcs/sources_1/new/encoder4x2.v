326cd9b386de08ddd82138f0499bd921561b39df
Jake Hafele <65046832+JakeHafele101@users.noreply.github.com>
2022-11-12 21:06:16 -0600

    added encoder code

---
+++ Starter Code/Starter Code.srcs/sources_1/new/encoder4x2.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/12/2022 08:33:33 PM
+// Design Name: 
+// Module Name: decoder2x4
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module encoder4x2(
+    input [3:0] X,
+    input en,
+    output [1:0] Y
+    );
+    
+    assign Y[0] = en&(X[1]|X[3]);
+    assign Y[1] = en&(X[2]|X[3]);
+    
+endmodule
